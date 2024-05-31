fc4ba1247a4fd935e017835ee3e6acb509b892aa
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-05-31 10:57:44 -0700

    Updated scripts.js to highlight the large Pokémon

--- js/scripts.js
+++ js/scripts.js
@@ -17,23 +17,38 @@ const pokemonList =
         height: 2,
         types: ['bug']
     }
-
 ];
-
 //created a for loop to write pokemon names and height to index.html//
-
 for (let i = 0; i < pokemonList.length; i++) {
-	document.write(
-        "<p>" + pokemonList[i].name +  " - This is a big Pokémon!!" + "</p>");
-}
-//created a loop to highlight Pokemon height info//
+    if (pokemonList[i].height > 5) {
+      document.write(
+        "<p>" + pokemonList[i].name + " - This is a big Pokémon!!" + "</p>",
+      );
+    }
     document.write(
-        "<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ")</p>",
+      "<p>" +
+        pokemonList[i].name +
+        " (height: " +
+        pokemonList[i].height +
+        ")</p>",
     );
+  }
 
-//added document write to add text to highlight the large pokémon//
-
-
+//alternnative solution
+//pokemonList.forEach((pokemon) => {
+   // if (pokemon.height > 5) {
+     // document.write(
+     //   "<p>" + pokemonList.name + " - This is a big Pokémon!!" + "</p>",
+   //   );
+   // }
+  //  document.write(
+    //  "<p>" +
+   //     pokemon.name +
+   //     " (height: " +
+   //     pokemon.height +
+   //     ")</p>",
+ //   );
+ // }
 
 
 
