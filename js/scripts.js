0cc5753a61a78a2233864aa05f5b0e0d99bcefd0
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-06-18 14:37:39 -0700

    Updated scripts.js to  add comments about what each of these are doing

--- js/scripts.js
+++ js/scripts.js
@@ -18,6 +18,8 @@ const pokemonRepository = (function () {
         types: ['bug', 'flying']
     }
 ];
+
+// Added a new Pokemon 
 function add(pokemon) {
   if (
     typeof pokemon === "object" &&
@@ -32,20 +34,23 @@ function add(pokemon) {
   }
 }
 
+// Function to get all Pokemon
 function getAll() {
 return pokemonList;
 }
-
+//Show Pokemon details 
 function showDetails (pokemon) {
   console.log(pokemon);
 };
 
+//Click event listener button added to display Pokemon details
 function onClick(clickedButton, pokemon) {
   clickedButton.addEventListener('click', function (event) {
   pokemonRepository.showDetails(pokemon);
   });
 }
 
+// Create and append pokemon list item with a Pokemon button
 function addListItem (pokemon) {
   let pokemonListElement = document.querySelector('.pokemon-list');
   let listItem = document.createElement('li');
@@ -56,25 +61,29 @@ function addListItem (pokemon) {
   pokemonListElement.appendChild(listItem);
   onClick(button, pokemon); 
 };
-
+// Retuns all 
 return {
   add: add,
   getAll: getAll,
-  addListItem: addListItem
+  addListItem: addListItem,
+  showDetails: showDetails
 };
-})();
 
+})();
+// Logs Pokemon in repository
 console.log(pokemonRepository.getAll());
 pokemonRepository.add({ name: "Pikachu", height: 0.3, types: ["electric"]
 });
 
+// Adds a new Pokemon to repository
 console.log(pokemonRepository.getAll());
 
+// Logs all Pokemon to the repository 
 pokemonRepository.getAll().forEach(function (pokemon) {
   pokemonRepository.addListItem(pokemon);
 });
 
-//shouldnot be added
+// This Pokemon should not be added
 pokemonRepository.add(    {
   name: "Butterfree3",
   height: 2,
