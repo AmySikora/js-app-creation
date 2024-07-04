fa1544da4bca1c8fd2f0cdb077ab650970990cbc
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-07-03 18:20:01 -0700

    Updated JS to add dialog functions and promises

--- js/scripts.js
+++ js/scripts.js
@@ -88,59 +88,102 @@ pokemonRepository.loadList().then(function () {
 // Add modal for Pokemen app
 
 (function() {
-  function showModal(title, text) {
   let modalContainer = document.querySelector('#modal-container');
-
-  // Clear all exisiting modal content
-  modalContainer.innerHTML = '';
+  let dialogPromiseReject; 
   
-  let modal = document.createElement('div');
-  modal.classList.add('modal');
+  function showModal(title, text) {
+    // Clear all existing modal content
+    modalContainer.innerHTML = '';
+    
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+    
+    // Add the new modal content
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+    
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = title;
+    
+    let contentElement = document.createElement('p');
+    contentElement.innerText = text;
+    
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
+    
+    modalContainer.classList.add('is-visible');
+  }
   
-  //Add the new model content
-  let closeButtonElement = document.createElement('button');
-  closeButtonElement.classList.add('modal-close');
-  closeButtonElement.innerText = 'Close';
-  closeButtonElement.addEventListener('click', hideModal);
+  function hideModal() {
+    modalContainer.classList.remove('is-visible');
 
-  let titleElement = document.createElement('h1');
-  titleElement.innerText = title;
-
-  let contentElement = document.createElement('p');
-  contentElement.innerText = text;
-
-  modal.appendChild(closeButtonElement);
-  modal.appendChild(titleElement);
-  modal.appendChild(contentElement);
-  modalContainer.appendChild(modal);
-
-  modalContainer.classList.add('is-visible');
-
-  modalContainer.addEventListener('click', (e) => {
-    // Since this is also triggered when clicking INSIDE the modal
-    // We only want to close if the user clicks directly on the overlay
-    let target = e.target;
-    if (target === modalContainer) {
-      hideModal();
+    if(dialogPromiseReject) {
+      dialogPromiseReject();
+      dialogPromiseReject = null
     }
+  }
+  
+  function showDialog(title, text) {
+    showModal(title, text);
+
+    // We want to add confirm and cancel buttons to the modal
+    let modal = modalContainer.querySelector('.modal');
+
+    let confirmButton = document.createElement('button');
+    confirmButton.classList.add('modal-confirm');
+    confirmButton.innerText = 'Confirm';
+
+    let cancelButton = document.createElement('button');
+    cancelButton.classList.add('modal-cancel');
+    cancelButton.innerText = 'Cancel';
+
+    modal.appendChild(confirmButton);
+    modal.appendChild(cancelButton);
+
+    // We want to focus the confirmButton so that the user can simply press Enter
+    confirmButton.focus();
+
+    // Return a promise that resolves when confirmed and rejects when not
+    return new Promise((resolve, reject) => { cancelButton.addEventListener('click',
+      hideModal);                                         
+    confirmButton.addEventListener('click', () => {
+      dialogPromiseReject = null; //reset this
+    hideModal();
+    resolve();
+    });
+// This can be used to reject other functions
+  dialogPromiseReject = reject;
+  });
+  }
+                                          
+  document.querySelector('#show-modal').addEventListener('click', () => {
+    showModal('Modal title', 'This is the modal content!');
   });
 
-  }
-
-  function hideModal() {
-  let modalContainer = document.querySelector('#modal-container');
-  modalContainer.classList.remove('is-visible');
-}
-
-window.addEventListener('keydown', (e) => {
-  let modalContainer = document.querySelector('#modal-container');
-  if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+  document.querySelector('#show-dialog').addEventListener('click', () => {
+    showDialog('Confirm action', 'Are you sure you want to do this?').then(function() {
+      alert('confirmed!');
+    }, () => {
+      alert('not confirmed');
+    });
+  });
+  
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
       hideModal();  
     }
   });
-
-document.querySelector('#show-modal').addEventListener('click', () => {
-  showModal('Modal title', 'This is the modal content!');
+  
+modalContainer.addEventListener('click', (e) => {
+    // Since this is also triggered when clicking INSIDE the modal container,
+    // We only want to close if the user clicks directly on the overlay
+    let target = e.target;
+    if (target === modalContainer) 
+      hideModal();
 });
 
 })();
