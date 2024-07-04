317a748a55b89a0ac21e7fbc4551a8b6f088162f
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-07-03 18:00:35 -0700

    added JS for modal functionality

--- js/scripts.js
+++ js/scripts.js
@@ -85,12 +85,45 @@ pokemonRepository.loadList().then(function () {
     pokemonRepository.addListItem(pokemon);
   });
 });
+// Add modal for Pokemen app
 
-function showModal () {
-  var modalContainer = document.querySelecter('#modal-container');
-  modalContainer.classList.add('is-visible');
-}
-
-document.querySelector('#show-modal').addEventListener('click', () => {
-  showModal();
+  (function() {
+    function showModal(title, text) {
+    let modalContainer = document.querySelector('#modal-container');
+  
+    // Clear all exisiting modal content
+    modalContainer.innerHTML = '';
+    
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+    
+    //Add the new model content
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+  
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = title;
+  
+    let contentElement = document.createElement('p');
+    contentElement.innerText = text;
+  
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
+  
+    modalContainer.classList.add('is-visible');
+  
+    modalContainer.addEventListener('click', (e) => {
+      // Since this is also triggered when clicking INSIDE the modal
+      // We only want to close if the user clicks directly on the overlay
+      let target = e.target;
+      if (target === modalContainer) {
+        hideModal();
+      }
+    });
+  
+    }
 })
